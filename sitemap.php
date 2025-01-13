52be477544492c80a3cda16af9b505d04c9e003d
Siael Alves <siaelalves@outlook.com>
2025-01-13 13:27:04 -0300

    REFACTOR Removidos arquivos obsoletos `const.php`, `get_tag.php`, `info_name.php`, `update.php` e `url_set.php` do módulo sitemap.

--- sitemap.php
+++ sitemap.php
@@ -8,14 +8,4 @@ require "CHANGE_FREQ.php" ;
 
 require "DEFAULTS.php" ;
 
-require "const.php" ;
-
-require "get_tag.php" ;
-
-require "info_name.php" ;
-
-require "url_set.php" ;
-
 require "refresh.php" ;
-
-require "update.php" ;
